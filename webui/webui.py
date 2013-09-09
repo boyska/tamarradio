aef66245dc2039241a88b17c4d44a55cdf3ba552
boyska <piuttosto@logorroici.org>
2013-09-10 01:34:05 +0200

    View all, view details (just draft)
    based on flask-bootstrap

--- webui/webui.py
+++ webui/webui.py
@@ -2,14 +2,20 @@ import sys
 import json
 from datetime import datetime
 
-from flask import Flask, Response
+from flask import Flask, Response, render_template, abort
+from flask_bootstrap import Bootstrap
 from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import joinedload
 
 sys.path.append('..')
 import tamarradb
-app = Flask(__name__)
+app = Flask(__name__,
+            template_folder='templates',
+            static_folder='static',
+            static_url_path='/static')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+bootstrap = Bootstrap(app)
 tamarradb.Base.metadata.create_all(db.engine)
 
 
@@ -25,21 +31,29 @@ def home():
         sorted((r.rule for r in app.url_map.iter_rules()))),
         mimetype='text/plain')
 
+#####
+# API
+#####
 
 @app.route("/api/event/view/<event_id>")
-def event_view(event_id):
+def api_event_view(event_id):
     return Response(json.dumps(list(db.session.query(tamarradb.Event).
                     filter(tamarradb.Event.id == event_id))),
                     mimetype='application/json')
 
 
 @app.route("/api/event/view")
-def event_view_all():
+def api_event_view_all():
     return Response(json.dumps(list(db.session.query(tamarradb.Event))),
                     mimetype='application/json')
 
 
-@app.route("/event/add")
+#####
+# TEST
+#####
+
+#TODO: remove me, I'm just an utility for quick test
+@app.route("/test/add")
 def event_add():
     e = tamarradb.Event('new')
     db.session.add(e)
@@ -48,6 +62,29 @@ def event_add():
     e.alarm = tamarradb.SingleAlarm(datetime.now())
     db.session.commit()
     return str(e.id)
+
+#####
+# User Interface
+#####
+
+
+@app.route('/event/view')
+def event_view_all():
+    events = list(
+        db.session.query(tamarradb.Event).
+        options(joinedload('alarm'),
+                joinedload('action')
+                )
+    )
+    return render_template('view_all.html', events=events)
+
+
+@app.route('/event/view/<event_id>')
+def event_view(event_id):
+    event = db.session.query(tamarradb.Event).filter(tamarradb.Event.id==event_id).first()
+    if event is None:
+        abort(404)
+    return render_template('view.html', event=event)
 
 
 @app.route("/event/edit/<id>")
