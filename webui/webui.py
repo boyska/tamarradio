fad62d10eefbb51d47332fb41767194474f8dd84
boyska <piuttosto@logorroici.org>
2013-09-10 11:04:13 +0200

    WEB style and filters

--- webui/webui.py
+++ webui/webui.py
@@ -9,6 +9,16 @@ from sqlalchemy.orm import joinedload
 
 sys.path.append('..')
 import tamarradb
+
+
+def display_tamarradb(obj):
+    if(isinstance(obj, tamarradb.Alarm)):
+        return "ALARM %s" % str(obj)
+    if(isinstance(obj, tamarradb.Action)):
+        return "ACTION %s" % str(obj)
+    return obj
+
+
 app = Flask(__name__,
             template_folder='templates',
             static_folder='static',
@@ -17,6 +27,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 tamarradb.Base.metadata.create_all(db.engine)
+app.jinja_env.filters['t_short'] = display_tamarradb
 
 
 def _json_default(self, obj):
@@ -27,13 +38,11 @@ json.JSONEncoder.default = _json_default  # replacement
 
 @app.route("/")
 def home():
-    return Response('Indirizzi validi: \n * %s' % '\n * '.join(
-        sorted((r.rule for r in app.url_map.iter_rules()))),
-        mimetype='text/plain')
-
+    return render_template('base.html', title='Tamarradio')
 #####
 # API
 #####
+
 
 @app.route("/api/event/view/<event_id>")
 def api_event_view(event_id):
@@ -81,7 +90,8 @@ def event_view_all():
 
 @app.route('/event/view/<event_id>')
 def event_view(event_id):
-    event = db.session.query(tamarradb.Event).filter(tamarradb.Event.id==event_id).first()
+    event = db.session.query(tamarradb.Event).\
+        filter(tamarradb.Event.id == event_id).first()
     if event is None:
         abort(404)
     return render_template('view.html', event=event)
@@ -90,8 +100,8 @@ def event_view(event_id):
 @app.route("/event/edit/<id>")
 def event_edit(event_id):
     event_id = int(event_id)
-
     return "welcome"
 
+
 if __name__ == "__main__":
-        app.run(debug=True)
+    app.run(debug=True)
